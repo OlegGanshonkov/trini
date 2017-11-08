dd2f7ee2c8ed2b82a88e424352ab8b760f099c81
olaccursed <olaccursed@yandex.ru>
2017-11-08 17:51:59 +0300

    main fix

--- views/site/index/contacts.php
+++ views/site/index/contacts.php
@@ -13,9 +13,9 @@
         <h2>Контакты</h2>
 
         <div class="items">
-            <div class="phone"><img src="/img/site/index/contacts/phone.png" /> +7 925 874-28-00</div>
-            <div class="email"><img src="/img/site/index/contacts/mail.png" /> v@trinitydigital.ru</div>
-            <div class="address"><img src="/img/site/index/contacts/address.png" /> г.Москва, ул.Бауманская, д.7<br>
+            <div class="phone"><img src="/img/site/index/contacts/phone.png" /> <a href="tel:+79258742800">+7 925 874-28-00</a></div>
+            <div class="email"><img src="/img/site/index/contacts/mail.png" /> <a href="mailto:v@trinitydigital.ru">v@trinitydigital.ru</a></div>
+            <div class="address"><img src="/img/site/index/contacts/address.png" /> <a target="_blank" href="https://yandex.ru/maps/213/moscow/?source=serp_navig&mode=search&text=%D0%B3.%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D1%83%D0%BB.%D0%91%D0%B0%D1%83%D0%BC%D0%B0%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%2C%20%D0%B4.7%20%D0%91%D0%A6%20%C2%ABCentral%20Yard%C2%BB%2C%20%D0%9E%D1%84%D0%B8%D1%81%20104&sll=37.676512%2C55.776452&ol=biz&oid=149698120907&ll=37.676512%2C55.776452&z=16&sspn=0.012746%2C0.006942&sctx=ZAAAAAgCEAAaKAoSCQO0rWadmUJAEUUOETenAExAEhIJwFlKlpNQoj8RvR5Mio9PoD8gACABIAIgAygFMAE4lfGQktLG56tTQO%2BRB0gBVQAAgD9YAGoCcnVwAA%3D%3D">г.Москва, ул.Бауманская, д.7</a><br>
                 БЦ «Central Yard», Офис&nbsp;104</div>
         </div>
         
@@ -25,4 +25,4 @@
             <a href="#">be</a>
         </div>
     </div>
-</section>
+<!--</section>-->
